8d9e20bae6ea3e9024b2952c2715f60f8a19032c
kaleby.cadorin <kaleby.cadorin@clevertech.biz>
2024-04-12 14:40:23 -0300

    initial commit

---
+++ scripts/sleep.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Log a message indicating the start of the pre-stop hook
+echo "Starting pre-stop hook..."
+
+# Log a message indicating the sleep duration
+echo "Sleeping for 30 seconds..."
+
+# Sleep for 30 seconds
+sleep 30
+
+# Log a message indicating the end of the pre-stop hook
+echo "Pre-stop hook completed."
